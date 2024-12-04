cff5eee134d5e8f35f082b694bc303f606d57642
MuhammadNasir1 <nmian7080@gmail.com>
2024-12-04 13:28:00 +0500

    add table

---
+++ app/Models/EnrollUnits.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EnrollUnits extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'course_id',
+        'reference_no',
+        'checked_status',
+        'submission_count',
+        'rejected_count',
+    ];
+    private $timestamp  = true;
+
+
+}
---
+++ database/migrations/2024_12_04_082229_create_enroll_units_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enroll_units', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('course_id');
+            $table->string('reference_no');
+            $table->integer('checked_status')->default(0);
+            $table->integer('submission_count')->default(0);
+            $table->integer('rejected_count')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enroll_units');
+    }
+};
